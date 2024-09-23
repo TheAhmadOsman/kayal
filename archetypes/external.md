6063eb52658725721b63627c97107ba2461ec3fb
Ahmad M. Osman <me@ahmadosman.com>
2024-09-23 06:53:17 -0500

    formatting

--- archetypes/external.md
+++ archetypes/external.md
@@ -2,7 +2,7 @@
 title: '{{ replace .File.ContentBaseName "-" " " | title }}'
 showDate: true
 showTags: true
-date: {{ .Date }}
+date: { { .Date } }
 externalUrl: ""
 summary: ""
 showReadingTime: false
--- assets/js/codecopy.js
+++ assets/js/codecopy.js
@@ -1,11 +1,13 @@
-var copyText = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path fill=\"currentColor\" d=\"M288 448L64 448l0-224 64 0 0-64-64 0c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l224 0c35.3 0 64-28.7 64-64l0-64-64 0 0 64zm-64-96l224 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L224 0c-35.3 0-64 28.7-64 64l0 224c0 35.3 28.7 64 64 64z\"/></svg>";
-var copiedText = "Copied! <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\"><path fill=\"currentColor\" d=\"M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z\"/></svg>";
+var copyText =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M288 448L64 448l0-224 64 0 0-64-64 0c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l224 0c35.3 0 64-28.7 64-64l0-64-64 0 0 64zm-64-96l224 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L224 0c-35.3 0-64 28.7-64 64l0 224c0 35.3 28.7 64 64 64z"/></svg>';
+var copiedText =
+  'Copied! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>';
 
 function createCopyButton(codeblock) {
   const container = codeblock.parentNode.parentNode;
 
-  const copybutton = document.createElement('button');
-  copybutton.classList.add('copy-button');
+  const copybutton = document.createElement("button");
+  copybutton.classList.add("copy-button");
   copybutton.innerHTML = copyText;
 
   function copyingDone() {
@@ -15,8 +17,8 @@ function createCopyButton(codeblock) {
     }, 500);
   }
 
-  copybutton.addEventListener('click', (cb) => {
-    if ('clipboard' in navigator) {
+  copybutton.addEventListener("click", (cb) => {
+    if ("clipboard" in navigator) {
       navigator.clipboard.writeText(codeblock.textContent);
       copyingDone();
       return;
@@ -28,9 +30,9 @@ function createCopyButton(codeblock) {
     selection.removeAllRanges();
     selection.addRange(range);
     try {
-      document.execCommand('copy');
+      document.execCommand("copy");
       copyingDone();
-    } catch (e) { };
+    } catch (e) {}
     selection.removeRange(range);
   });
 
@@ -38,9 +40,14 @@ function createCopyButton(codeblock) {
     container.appendChild(copybutton);
   } else if (container.parentNode.firstChild == container) {
     // td containing LineNos
-  } else if (codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.nodeName == "TABLE") {
+  } else if (
+    codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.nodeName ==
+    "TABLE"
+  ) {
     // table containing LineNos and code
-    codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(copybutton);
+    codeblock.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(
+      copybutton,
+    );
   } else {
     // code blocks not having highlight as parent class
     codeblock.parentNode.appendChild(copybutton);
@@ -48,5 +55,7 @@ function createCopyButton(codeblock) {
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelectorAll("pre > code").forEach((codeblock) => createCopyButton(codeblock));
+  document
+    .querySelectorAll("pre > code")
+    .forEach((codeblock) => createCopyButton(codeblock));
 });
